ae38ed2c5c0eafdf736fd30e184e6c68b4fb8afb
alex boyle <alexjamesboyle@gmail.com>
2020-12-05 00:45:49 -0600

    Day 5

--- solutionFiles/Day05_pt1.js
+++ solutionFiles/Day05_pt1.js
@@ -1,36 +1,21 @@
 module.exports  = function(input) {
-	let parse =function(input) {
+	let parse = function(input) {
 		let output = []
 		for(var i = 0; i < input.length-1; i ++) {
-			
-			let rowNum = input[i].substring(0,7);
-			let seatNum = input[i].substring(7);
-			
-			rowNum = rowNum.replace(/F/g, 0)
-			rowNum = rowNum.replace(/B/g, 1)
-
-			seatNum = seatNum.replace(/L/g, 0)
-			seatNum = seatNum.replace(/R/g, 1)
-
-			rowNum = parseInt(rowNum, 2)
-			seatNum = parseInt(seatNum, 2)
-
+			let rowNum = parseInt(input[i].substring(0,7).replace(/F/g, 0).replace(/B/g, 1), 2)
+			let seatNum = parseInt(input[i].substring(7).replace(/L/g, 0).replace(/R/g, 1), 2)
 			output.push(rowNum * 8 + seatNum)
 		}
 		return output
 	}
 	
-	var parsedInput = parse(input).sort((a,b)=> a-b);
-	console.log("The largetst seat Id is " + parsedInput[parsedInput.length-1])
-	console.log("")
-
-	var wasLastIncluded = true;
-	for (var i = parsedInput[0]; i < parsedInput[parsedInput.length-1]; i ++) {
-		if(!parsedInput.includes(i)) {
+	var seatIds = parse(input).sort((a,b)=> a-b);
+	console.log("The largetst seat Id is " + seatIds[seatIds.length-1])
+	
+	for (var i = seatIds[0]; i < seatIds[seatIds.length-1]; i ++) {
+		if(!seatIds.includes(i)) {
 			console.log("Your seat id is " + i);
 			break;
 		}
 	}
-
-
 }
