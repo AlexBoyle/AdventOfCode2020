650fb1ec73ce464ef2345de5591577173b781dcd
alex boyle <alexjamesboyle@gmail.com>
2020-12-08 00:09:01 -0600

    Day 8

---
+++ solutionFiles/Day08.js
@@ -0,0 +1,55 @@
+module.exports  = function(input) {
+	let parse = function(input) {
+		return input.reduce((out, line) => {
+			let sp = line.split(' ')
+			out.push({"ins": sp[0], 'num': parseInt(sp[1])})
+			return out
+		}, [])
+	}
+	let runGameboyDebugger = function(instructionSet, changeIndex) {
+		let accumulator = 0;
+		let instructionIndex = 0;
+		let instructionVisit = new Set([])
+		let runHealth = false;
+		let badInstList = new Set([])
+		while(!(instructionVisit.has(instructionIndex) || instructionIndex > instructionSet.length)) {
+			if(instructionIndex == instructionSet.length) {
+				runHealth = true;
+				break;
+			}
+			instructionVisit.add(instructionIndex)
+			let inst = instructionList[instructionIndex].ins
+			if(changeIndex != null && changeIndex == instructionIndex) {
+				if(inst == 'jmp')
+					inst = 'nop'
+				else if(inst == 'nop')
+					inst = 'jmp'
+			}
+			switch(inst) {
+				case 'acc':
+					accumulator += instructionSet[instructionIndex].num
+					instructionIndex ++;
+					break;
+				case 'jmp':
+					badInstList.add(instructionIndex)
+					instructionIndex += instructionSet[instructionIndex].num
+					break;
+				case 'nop':
+					badInstList.add(instructionIndex)
+					instructionIndex ++;
+					break;
+			}
+		}
+		return {"health": runHealth, "accumulator": accumulator, "possibleBadInstSet": [...badInstList]};
+	}
+	let instructionList = parse(input);
+	let debugOutput = runGameboyDebugger(instructionList)
+	console.log("Gameboy is ending badly with an accumulation of " + debugOutput.accumulator)
+	let debugOutputFixAttempt = null;
+	for(var i = 0; i < debugOutput.possibleBadInstSet.length; i ++) {
+		if((debugOutputFixAttempt = runGameboyDebugger(instructionList, debugOutput.possibleBadInstSet[i])).health) {
+			console.log("Gameboy is ending correctly with an accumulation of " + debugOutputFixAttempt.accumulator);
+			break;
+		}
+	}
+}
