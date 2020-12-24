e9c051b18b7d26a7272d3447acc02acfddd31089
alex boyle <alexjamesboyle@gmail.com>
2020-12-24 02:44:58 -0600

    Day 24

---
+++ solutionFiles/Day23.js
@@ -0,0 +1,131 @@
+module.exports  = function(input) {
+	let parse = function(input) {
+		return input[0].split('').reduce((out, line) => {
+			out.push(parseInt(line))
+			return out;
+		}, [])
+	}
+	let Link = function(num){this.num = num;this.next = null; this.prev = null;}
+	let toLinkedList = function(arr) {
+		let map = new Map()
+		let first = new Link(arr[0])
+		map.set(arr[0],first)
+		let prev = first
+		let next;
+		for(var i = 1; i < arr.length; i ++) {
+			next = new Link(arr[i])
+			map.set(arr[i],next)
+			next.prev = prev;
+			prev.next = next;
+			prev = next;
+		}
+		first.prev = next;
+		next.next = first;
+		//let tempCup = first.prev;while((tempCup = tempCup.next) != first.prev){console.log(tempCup.num)}console.log(tempCup.num)
+		return [{'selectedNum': first},map]
+	}
+	let cutCups = function(cupList, index, num) {
+		let out = []
+		let indexmod = 0;
+		if(index+2 >= cupList.length ){
+			for(var i = 0; i < num; i++ ){
+				if((index)%cupList.length == 0)
+					index=0
+				out.push(cupList.splice((index+indexmod)%cupList.length,1)[0])
+			}
+		}
+		else {
+		out = cupList.splice(index,3)
+		}
+		return out
+	}
+	let cutCups1 = function(cup) {
+			let cutStart = cup.next;
+			let cutLast = cup.next.next.next;
+			
+			cup.next = cup.next.next.next.next;
+			cutLast.next.prev = cup
+			
+			cutLast.next = null
+			cutStart.prev = null;
+			
+			return [cutStart,cutLast]
+
+		return out
+	}
+	let printArr = function(arr) {
+		let str = "["
+		try {
+		let tempCup = arr.prev;while((tempCup = tempCup.next) != arr.prev && tempCup != null){str += tempCup.num + " "}str += tempCup.num + " "
+		} catch(e){console.log('print Failed')}
+		console.log(str)
+	}
+	let printArrB = function(arr) {
+		let str = "["
+		try {
+		let tempCup = arr.next;while((tempCup = tempCup.prev) != arr.next && tempCup != null){str += tempCup.num + " "}str += tempCup.num + " "
+		} catch(e){console.log('print Failed')}
+		console.log(str)
+	}
+	let game = function(cupList,num, maxNum, map) {
+		let currentcupIndex = 0;
+		let dest = 1;
+		let pickedup
+		let indexToInsert;
+		for(var i = 1; i <= num; i ++) {
+			//console.log(cupList.selectedNum)
+			//printArr(cupList.selectedNum)
+			pickedup = cutCups1(cupList.selectedNum)
+			let find = cupList.selectedNum.num
+			/////////////////////////////////////////////////
+			let insertNextto;
+			let found = false;
+			while(!found) {
+				find --
+			if(find == 0){find = maxNum}
+			try{
+				let t = map.get(find);
+				if(t.next.next.next.next.next.next) {
+					found = true;
+					insertNextto = t
+				}
+			}catch(e){}
+			}
+			
+			///////////////////////////////////
+			let tempCup = insertNextto.next;
+			//console.log(insertNextto)
+			insertNextto.next = pickedup[0]
+			pickedup[0].prev = insertNextto
+			//
+			tempCup.prev = pickedup[1]
+			pickedup[1].next = tempCup
+			//
+			//printArr(cupList.selectedNum)
+			cupList.selectedNum = cupList.selectedNum.next
+			
+		}
+		let t = map.get(1);
+		return [cupList.selectedNum,t.next.num*t.next.next.num]
+	}
+	
+	{
+		let out = parse(input)
+		let linkedArr,map;
+		[linkedArr,map] = toLinkedList(out)
+		let partOut = game(linkedArr,100, out.length, map)
+		console.log('Pt1: ')
+		printArr(partOut[0])
+	}
+	{
+		let out = parse(input)
+		for(var i = out.length+1; i <= 1000000; i ++) {
+			out.push(i)
+		}
+		
+		let linkedArr,map;
+		[linkedArr,map] = toLinkedList(out)
+		partOut = game(linkedArr,10000000, out.length, map)
+		console.log('Pt2: ' + partOut[1])
+	}
+}
---
+++ solutionFiles/Day24.js
@@ -0,0 +1,183 @@
+const matchAll = require("match-all");
+module.exports  = function(input) {
+	const BLACK = true;
+	const WHITE = false;
+	let Vector = function(x,y,z,w){this.x = x; this.y = y; this.z = z;this.w = w}
+	let Tile = function(val, x, y){this.val = val; this.y = y; this.x = x;}
+	let parse = function(input) {
+		return [...input].reduce((out, line) => {
+			let parsed = []
+			while (line.length > 0){
+				let match = line.match(/^((?:se|sw|nw|ne)|(?:w|e))/g)[0]
+				parsed.push(match)
+				line = line.split('')
+				line.splice(0,match.length )
+				line = line.join('')
+			}
+			out.push(parsed)
+			return out;
+		}, [])
+	}
+	let traverse = function(directions) {
+		let pos = {'x':0, 'y':0}
+		directions.forEach((direction) =>{
+			switch(direction) {
+				case 'e': 
+					pos.x++
+					break;
+				case 'w':
+					pos.x--;
+					break;
+				case 'ne':
+					pos.y--
+					pos.x++
+					break;
+				case 'nw':
+					pos.y--
+					break;
+				case 'se':
+					pos.y++
+					break;
+				case 'sw':
+					pos.y++
+					pos.x--
+					break;
+				
+			}
+		})
+		return pos
+	}
+	let countTiles = function(tileMap) {
+		let blackCount = 0
+		let whiteCount = 0
+		for (const [key, value] of tileMap.entries()) {
+			if(value.val == BLACK) {
+				blackCount++
+			}
+			else {
+				whiteCount ++
+			}
+		}
+		return [whiteCount, blackCount]
+	}
+	let closeTiles = [
+		new Vector(-1,0),
+		new Vector(-1,1),
+		
+		new Vector(0,-1),
+		new Vector(0,1),
+		
+		new Vector(1,-1),
+		new Vector(1,0),
+	]
+	let countCloseBlackTiles = function(tile, tileMap) {
+		let count = 0;
+		closeTiles.forEach((vector) => {
+			try {
+				let key = (tile.x+vector.x) + ":" + (tile.y+vector.y)
+				if(tileMap.has(key) && tileMap.get(key).val == BLACK){count++;}
+			}catch(e){}
+		})
+		return count;
+	}
+	let part1 = function(input) {
+		let flipedTiles = new Map()
+		input.forEach((directions) => {
+			let pos = traverse(directions)
+			let key = pos.x + ":" + pos.y
+			if(flipedTiles.has(key)) {
+				let tile = flipedTiles.get(key)
+				tile.val = !tile.val
+			}else {
+				flipedTiles.set(key, new Tile(BLACK, pos.x,pos.y))
+			}
+		})
+		let counts = countTiles(flipedTiles)
+		
+		console.log('Pt1: ' + counts[1])
+		return flipedTiles
+	}
+	let updateTile = function(x,y,oldMap, tileMap) {
+		let key = x + ":" + y;
+		let tile;
+		let tag = "old "
+		let count
+		if(!oldMap.has(key)) {
+			
+			tile = new Tile(WHITE,x,y)
+			count = countCloseBlackTiles(tile, oldMap)
+			if(count == 2) {
+				tileMap.set(key,  tile)
+			}else {return}
+			tag = "new "
+		} else {
+			tile = tileMap.get(key)
+			count = countCloseBlackTiles(tile, oldMap)
+		}
+		//console.log(tag + count + (tile.val == BLACK? " black ": " white ") + key)
+		if(tile.val == BLACK && count != 1 && count != 2) {
+			//console.log(key + " is set to white");
+			tile.val = WHITE;
+			//console.log(" white")
+			return;
+		}//if(tile.val == BLACK){console.log(key + " is still black");return}
+		if(tile.val == WHITE && (count == 2)) {
+			tile.val = BLACK;
+			//console.log(key + " is set to black");
+			//console.log(" black")
+			return;
+		}//if(tile.val == WHITE){console.log(key + " is still white");return}
+		//console.log((tile.val == BLACK? " black": " white"))
+		
+	}
+	let getTileKey = function(tile,x=0,y=0) {
+		return (tile.x+x) + ":" + (tile.y+y)
+	}
+	let dupMap = function(map) {
+		let newMap = new Map()
+		for (const [key, value] of map.entries()) { 
+			newMap.set(key,new Tile(value.val, value.x,value.y))
+		}
+		return newMap
+	}
+	let part2 = function(initPos, num) {
+		let oldTileMap = dupMap(initPos)
+		/////////////////////////////////
+		console.log('Day ' + (0) + ": " + countTiles(initPos))
+		for(var i = 0; i < num; i ++) {
+			let lX=0,sX=0,lY=0;sY=0;
+			let tileMap = dupMap(oldTileMap)
+			let updated = new Set();
+			for (const [key, value] of oldTileMap.entries()) {
+				let key = getTileKey(value)
+				if(!updated.has(key)){updated.add(key);updateTile(value.x,value.y,oldTileMap, tileMap)};
+				if(value.x > lX)(lX = value.x);if(value.y > lY)(lY = value.y);if(value.x < sX)(sX = value.x);if(value.y < sY)(sY = value.y);
+				if(true) {
+					closeTiles.forEach((vector) => {
+						key = (value.x+vector.x) + ":" + (value.y+vector.y)
+						if((value.x+vector.x) > lX)(lX = (value.x+vector.x));if((value.y+vector.y) > lY)(lY = (value.y+vector.y));if((value.x+vector.x) < sX)(sX =(value.x+vector.x));if((value.y+vector.y) < sY)(sY = (value.y+vector.y));
+						if(!updated.has(key)){updated.add(key);updateTile(value.x+vector.x,value.y+vector.y,oldTileMap, tileMap)}
+					})
+				}
+			}
+			//console.log('xBound: [' + sX +', ' + lX+'] yBound: [' + sY + ', ' + lY +']')
+			oldTileMap = dupMap(tileMap)
+			console.log('Day ' + (i+1) + ": " + countTiles(tileMap))
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	let parseddata = parse(input)
+	let initPos = part1(parseddata)
+	part2(initPos, 100)
+}
+//149576328
